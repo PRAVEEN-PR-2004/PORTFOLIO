7ac473ceed17388e22e717df5a7619e8390dc0f3
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2025-05-19 18:18:20 +0530

    seo

--- src/Component/WorkCart.js
+++ src/Component/WorkCart.js
@@ -7,24 +7,24 @@ import { Slide } from "react-awesome-reveal";
 function WorkCart() {
   return (
     <Slide duration={1000} triggerOnce>
-    <div className="work-container " >
-      <h1 className="project-heading ">PROJECTS</h1>
-      
-      <div className="project-container ">
-        {ProjectData.map((val, ind) => {
-          return (
-            <MainCart
-              key={ind}
-              imgsrc={val.imgsrc}
-              title={val.title}
-              text={val.text}
-              view={val.view}
-              source={val.source}
-            />
-          );
-        })}
+      <div className="work-container ">
+        <h1 className="project-heading ">PROJECTS</h1>
+
+        <div className="project-container ">
+          {ProjectData.map((val, ind) => {
+            return (
+              <MainCart
+                key={ind}
+                imgsrc={val.imgsrc}
+                title={val.title}
+                text={val.text}
+                view={val.view}
+                source={val.source}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
     </Slide>
   );
 }
